90cccdc34b833a974f5bbc1f7c8d5b1d10a90075
priyangshupalis17 <49491590+priyangshupalis17@users.noreply.github.com>
2019-04-29 02:56:39 +0530

    Add files via upload

--- subset.cpp
+++ subset.cpp
@@ -1,79 +1,74 @@
 #include<iostream>
-#include<algorithm>
-#include<stdio.h>
+#include<vector>
 using namespace std;
-int arr[50],n,sum,inc[50];
-
-int promising(int i,int w,int total)
-{
-    return ((w+total)>=sum && ((w==sum)||(w+arr[i+1]<=sum)));
-}
-
-void subset(int i,int w,int total)
-{
-    if(promising(i,w,total))
-    {
-        if(w==sum)
-        {
-            cout<<"{\t";
-            for(int i=0;i<n;i++)
-                if(inc[i])
-                    cout<<arr[i]<<" ";
-            cout<<"\t}";
-        }
-        else
-        {
-            inc[i+1]=1;
-            subset(i+1,w+arr[i+1],total-arr[i+1]);
-            inc[i+1]=0;
-            subset(i+1,w,total-arr[i+1]);
-        }
-    }
-}
+vector<int> a;	
+int nsol=0,d;	
+		
+void with(vector<int> sub,int sr,int sc,int i);
+void without(vector<int> sub,int sr,int sc,int i);
+int sum(vector<int> sub);
 int main()
 {
-    int temp,total=0;
-    cout<<"enter no.of elemnts\n";
+    int n,i,val;	
+    vector<int> sub;	
+    cout<<"Enter the number of elements in the set:";
     cin>>n;
-    cout<<"enter the elemnts\n";
-    for(int i=0;i<n;i++)
+    cout<<"\nEnter the elements:";
+    for(i=0;i<n;i++)
     {
-        cin>>arr[i];
-        total+=arr[i];
+        cin>>val;
+        a.push_back(val);
     }
-    cout<<"enter sum\n";
-    cin>>sum;
-    for (int i=0;i<n;i++)
-	  for (int j=0;j<n-1;j++)
-	   if(arr[j]>arr[j+1]) {
-		temp=arr[j];
-		arr[j]=arr[j+1];
-		arr[j+1]=temp;
-	}
-    for(int i=0;i<n;i++)
-    {
-        cout<<arr[i]<<" ";
-    }
-    cout<<"\n"<<total<<endl;
-        if(total<sum)
-            cout<<"subset not possible\n";
-        else
-        {
-            for(int i=0;i<n;i++)
-                inc[i]=0;
-            subset(-1,0,total);
-        }
+    cout<<"\nEnter the maximum sum of the subsets:";
+    cin>>d;
+    with(sub,sum(a),0,0);	
+    without(sub,sum(a),0,0);  
+    cout<<"\nTotal number of possible subsets: "<<nsol;
+    return 0;
 }
 
-/* OUTPUT:
-enter no.of elemnts
-6
-enter the elemnts
-5 10 15 12 16 18
-enter sum
-30
-5 10 12 15 16 18 
-76
-{	5 10 15 	}{	12 18 	}
-*/
+void with(vector<int> sub,int sr,int sc,int i)
+{
+    if(sr+sc<d || i>=a.size())
+        return;
+    int j;
+    sr=sr-a[i];
+    sc=sc+a[i];
+    sub.push_back(a[i]);
+    if(sum(sub)==d)
+    {
+        cout<<"\nSubset "<<++nsol<<":\n";
+        for(j=0;j<sub.size();j++)
+            cout<<sub[j]<<" ";
+        return;
+    }
+    with(sub,sr,sc,i+1);	
+    without(sub,sr,sc,i+1);  
 
+}
+void without(vector<int> sub,int sr,int sc,int i)
+{
+    if(sr+sc<d || i>=a.size())
+        return;
+    if(sc>d)
+        return;
+    int j;
+    sr=sr-a[i];
+    sc=sc+a[i];
+    if(sum(sub)==d)
+    {
+        cout<<"\nSubset "<<++nsol<<":\n";
+        for(j=0;j<sub.size();j++)
+            cout<<sub[j]<<" ";
+        return;
+    }
+    with(sub,sr,sc,i+1);	
+    without(sub,sr,sc,i+1);	
+}
+int sum(vector<int> sub)	
+{
+    int sum=0,i;
+    for(i=0;i<sub.size();i++)
+        sum+=sub[i];
+    return sum;
+}
